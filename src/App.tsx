58786ce67415738352b5ee5cc4cb45768604df37
surivig18 <surivig18@gmail.com>
2021-10-08 17:29:12 +0800

    Latest changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import fun from './assets/image.gif'
 import happycustomers from './assets/200.gif'
 import panda from './assets/panda.gif'
+import collab from './assets/comedysportz-comedy.gif'
 import './App.css';
 import {Container, Nav, Navbar, Row, Col, Image, Card, Button} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -38,23 +39,26 @@ function Home() {
       }>
         <Container style={{padding : "100px 0 100px 0"}}>
           <Row>
-            <Col xs={7} md={3}>
-              <Image src={fun} thumbnail/>
+            <Col xs={5} md={3}>
+              <Image src={fun} style={{width: '100%',height : "80%"}} thumbnail/>
             </Col>
-            <Col xs={6} md={4}>
-              <Image src={happycustomers} thumbnail/>
+            <Col xs={5} md={3}>
+              <Image src={happycustomers} style={{width: '100%',height : "80%"}} thumbnail/>
             </Col>
-            <Col xs={6} md={4}>
-              <Image src={panda} thumbnail/>
+            <Col xs={5} md={3}>
+              <Image src={panda} style={{width: '100%',height : "80%"}} thumbnail/>
+            </Col>
+            <Col xs={5} md={3}>
+              <Image src={collab} style={{width: '100%'}} thumbnail/>
             </Col>
           </Row>
         </Container>
         <Container style={{paddingBottom : "100px"}}>
           <Row>
-            <Col xs={7} md={3}>
+            <Col xs={5} md={3}>
               <Card style={{width: '18rem'}}>
                 <Card.Body>
-                  <Card.Title>Making work as enjoyable for employees</Card.Title>
+                  <Card.Title>Making work fun</Card.Title>
                   <Card.Text>
                     <p>It's a tough 2 years for almost all of us and PALO makes us feeling belonged
                     with the Honey Fridays and other goodies every month. </p>
@@ -63,23 +67,34 @@ function Home() {
                 </Card.Body>
               </Card>
             </Col>
-            <Col xs={6} md={4}>
+            <Col xs={5} md={3}>
               <Card style={{width: '18rem'}}>
                 <Card.Body>
                   <Card.Title>Happy Clients</Card.Title>
                   <Card.Text>
-                    The quality that PALO ensures in its work makes the customers come back for more work
+                    The quality that PALO ensures in its work makes the client come back for more work
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
-            <Col xs={6} md={4}>
+            <Col xs={5} md={3}>
               <Card style={{width: '18rem'}}>
                 <Card.Body>
-                  <Card.Title>Share and Care</Card.Title>
+                  <Card.Title>Sharing is Caring</Card.Title>
                   <Card.Text>
                     <p> The KS sessions, design thinking, technical hive meetings really help us grow personally and also on a technical level.</p>
                      <p> Also gives a chance to bond ourselves in this time</p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={5} md={3}>
+              <Card style={{width: '18rem'}}>
+                <Card.Body>
+                  <Card.Title>Collaboration and Mentorship</Card.Title>
+                  <Card.Text>
+                    <p> Pairing with colleagues for work which is something I enjoy at PALO</p>
+                    <p> Had a great chance to mentor and also conduct interviews</p>
                   </Card.Text>
                 </Card.Body>
               </Card>
